091fef05f4e740ba0ef7238ca420bfb33225fe50
薛勇康 <xueyongkang@tongyangstock.com>
2019-12-23 15:33:08 +0800

    1.spring cloud alibaba sidecar 结合非spring cloud 微服务模式  例子 node.js服务 2.更新readMe

---
+++ ms-sidecar/src/main/java/com/mloine/mssidecar/MsSidecarApplication.java
@@ -0,0 +1,13 @@
+package com.mloine.mssidecar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSidecarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSidecarApplication.class, args);
+    }
+
+}
---
+++ ms-sidecar/src/test/java/com/mloine/mssidecar/MsSidecarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mloine.mssidecar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSidecarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
